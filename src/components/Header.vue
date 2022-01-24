f0da2c197fdb8f04693d8f8c2d1d656fa45f2455
otuoio <mariocoppo91@gmail.com>
2022-01-24 11:33:45 +0100

    add header structur

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,22 @@
 <template>
   <header>
       header
+      <!-- header bar logo menu (da metter con absolute) -->
+      <div class="header-top">
+          <div class="logo"></div>
+          <nav class="nav-menu">
+              <ul>
+                  <li></li>
+              </ul>
+          </nav>
+      </div>
+      <!-- /header bar logo menu -->
+      <!-- banner-box -->
+      <section class="banner-box">
+          <div class="info-box"></div>
+          <div class="img-box"></div>
+      </section>
+      <!-- /banner-box -->
   </header>
 </template>
 
