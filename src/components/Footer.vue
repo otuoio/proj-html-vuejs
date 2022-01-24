914e87d8b7fce74d85dd0b7504497789ea440563
otuoio <mariocoppo91@gmail.com>
2022-01-24 12:07:26 +0100

    add footer structure

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,16 @@
 <template>
   <footer>
       footer
+      <div class="container-40">
+          <div class="logo"></div>
+          <p class="disclaim"></p>
+          <div class="social">
+              <ul class="menu-inline">
+                  <li></li>
+              </ul>
+          </div>
+      </div>
+      <p class="copyright"></p>
   </footer>
 </template>
 
