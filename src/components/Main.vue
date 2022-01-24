b907ac046ef48afaa591f6ea4bc6fc3dfe2bf4e7
otuoio <mariocoppo91@gmail.com>
2022-01-24 11:52:55 +0100

    add main structure

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,94 @@
 <template>
   <main>
       main
+      <!-- jumbotron -->
+      <section class="jumbo">
+          <!-- centered-banner-box -->
+          <div class="centered-banner-box">
+              <div class="col-left"></div>
+              <div class="col-right"></div>
+          </div>
+          <!-- /centered-banner-box -->
+
+          <!-- call to action box -->
+          <div class="cta-box">
+              <div class="col"></div>
+              <div class="col"></div>
+              <div class="col"></div>
+          </div>
+          <!-- /call to action box -->
+      </section>
+      <!-- /jumbotron -->
+
+      <!-- banner-box -->
+      <section class="banner-box">
+          <div class="img-box"></div>
+          <div class="info-box"></div>
+      </section>
+      <!-- /banner-box -->
+
+      <!-- critics -->
+      <section class="critics">
+          <h2 class="sec-title"></h2>
+
+          <!-- centered-banner-box -->
+          <div class="centered-banner-box">
+              <div class="col-left"></div>
+              <div class="col-right"></div>
+          </div>
+          <!-- /centered-banner-box -->
+
+          <!-- centered-banner-box -->
+          <div class="centered-banner-box">
+              <div class="col-left"></div>
+              <div class="col-right"></div>
+          </div>
+          <!-- /centered-banner-box -->
+
+          <!-- full screen banner -->
+          <div class="fs-banner">
+              <div class="picture"></div>
+          </div>
+          <!-- /full screen banner -->
+      </section>
+      <!-- /critics -->
+
+      <!-- news & promotions -->
+      <section class="promo">
+          <h2 class="sec-title"></h2>
+
+          <!-- call to action box -->
+          <div class="cta-box">
+              <div class="col"></div>
+              <div class="col"></div>
+              <div class="col"></div>
+          </div>
+          <!-- /call to action box -->
+      </section>
+      <!-- /news & promotions -->
+
+      <!-- banner-box -->
+      <section class="banner-box">
+          <div class="img-box"></div>
+          <div class="info-box"></div>
+      </section>
+      <!-- /banner-box -->
+
+      <!-- call to action box -->
+        <div class="cta-box">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+      <!-- /call to action box -->
+
+      <!-- full screen banner -->
+        <div class="fs-banner">
+            <div class="picture"></div>
+            <div class="info-box"></div>
+        </div>
+      <!-- /full screen banner -->
+
   </main>
 </template>
 
